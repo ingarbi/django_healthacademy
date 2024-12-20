a3ea410c24169cfe5e4e54738353cde23895ad4d
ingarbi <104501854+ingarbi@users.noreply.github.com>
2024-12-20 22:08:58 +0300

    models doctor, patient, base

--- patient/models.py
+++ patient/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+from base.models import Appointment
+from userauths.models import User
+
+NOTIFICATION_CHOICES = (
+    ("Запись создана", "Запись создана"),
+    ("Запись отменена", "Запись отменена"),
+)
+
+
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.FileField(upload_to="images", null=True, blank=True)
+    full_name = models.CharField(max_length=100, null=True, blank=True)
+    email = models.CharField(max_length=100, null=True, blank=True)
+    mobile = models.CharField(max_length=100, null=True, blank=True)
+    address = models.CharField(max_length=100, null=True, blank=True)
+    gender = models.CharField(max_length=100, null=True, blank=True)
+    blood_group = models.CharField(max_length=100, null=True, blank=True)
+    dob = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.full_name
+
+
+class Notification(models.Model):
+    patient = models.ForeignKey(
+        Patient, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    appointment = models.ForeignKey(
+        Appointment,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="doctor_appointment_notification",
+    )
+    category = models.CharField(
+        max_length=100,
+        choices=NOTIFICATION_CHOICES,
+    )
+    seen = models.BooleanField(default=False)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Доктор {self.patient.full_name} - Уведомление"
